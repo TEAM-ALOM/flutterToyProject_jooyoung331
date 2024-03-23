529988052fc4afdfc05679daa90e7f4051574f5b
jooyoung_331 <ljy040331@gmail.com>
2024-03-23 15:30:22 +0900

    add keyboard detector and memo border

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:toyproject/screens/homescreen.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  //Hive.registerAdapter(TodoAdapter());
   runApp(const App());
 }
 
